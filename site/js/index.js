af04868dfa709a532c96156c5bffd02877719825
Nehe Vishakha <vishakhag6@gmail.com>
2018-05-02 14:38:00 +0530

    server file added and API integration

---
+++ site/js/index.js
@@ -0,0 +1,177 @@
+var app = angular.module('indexApp',['commonService', 'heroBanner']);
+app.controller('indexCtrl', ['$scope', 'indexService', '$timeout', function($scope, $indexService, $timeout) {
+
+	$scope.mainHeadingMessage = $indexService.mainHeading; // Heading form service(data sharing)
+    $scope.student = {};
+
+    $scope.$on('onFinishRender',function(){
+    	$('select').formSelect();
+	});
+
+    $scope.studentArr = [];
+    var studentUpdate = {};
+    $scope.booleanObj = {
+        showSaveBtn : true,
+        showUpdateBtn : false,
+        showCancelBtn : false,
+        showEditBtn : true,
+        showDeleteBtn : true
+    };
+
+	$scope.showDeletedMsz = false;
+
+	$scope.searchText = "";
+
+    $scope.gender = [ "female", "male"]; // Select array
+
+	$scope.indexVar = "";
+
+	$scope.userDataObj = [
+		'http://res.cloudinary.com/vishakhanehe/image/upload/v1491620696/san_myugdd.jpg',
+		'http://res.cloudinary.com/vishakhanehe/image/upload/v1491620687/paris_rytltc.jpg',
+		'http://res.cloudinary.com/vishakhanehe/image/upload/v1491620686/NY_arfrud.jpg'
+	];
+
+    // Save functionality
+    $scope.saveFunc = function(){
+        $scope.studentArr = $indexService.save($scope.studentArr, $scope.student);
+
+		// Store in local storage
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem("studentArr", JSON.stringify($scope.studentArr));
+		}
+        $scope.student = {};
+    };
+
+	// Delete functionality(On the basis of Modal)
+	$scope.showDeleteModal = function(index){
+		var elem = document.querySelector('#deleteModal');
+		var instance = M.Modal.init(elem);
+		instance.open();
+
+		$scope.deleteFunc = function(){
+			$scope.studentArr = $indexService.delete($scope.studentArr, index);
+
+			// delete a particular value from object
+			if (typeof(Storage) !== "undefined") {
+				localStorage.removeItem("studentArr");
+				localStorage.setItem("studentArr", JSON.stringify($scope.studentArr));
+			}
+			$scope.showDeletedMsz = true;
+			$timeout(function(){
+				$scope.showDeletedMsz = false;
+			}, 3000);
+
+			instance.close();
+		};
+	};
+
+    // Edit functionality
+    $scope.onEditFunc = function(student, index) {
+        studentUpdate = angular.copy(student);
+        $scope.student = studentUpdate;
+		$scope.indexVar = index;
+        $scope.booleanObj = {
+            showSaveBtn : false,
+            showUpdateBtn : true,
+            showCancelBtn : true,
+            showEditBtn : false,
+            showDeleteBtn : false
+        };
+    };
+
+    // Update functionality
+    $scope.updateFunc = function(student, index) {
+        $scope.studentArr = $indexService.update($scope.studentArr, student, index);
+        console.log(index);
+		// Update in local storage
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem("studentArr", JSON.stringify($scope.studentArr));
+		}
+        $scope.student = {};
+        $scope.booleanObj = {
+            showSaveBtn : true,
+            showUpdateBtn : false,
+            showCancelBtn : false,
+            showEditBtn : true,
+            showDeleteBtn : true
+        };
+    };
+
+    // Cancel functionality
+    $scope.onCancelFunc = function() {
+		var elem = document.querySelector('#deleteModal');
+		var instance = M.Modal.init(elem);
+		instance.close();
+        $scope.student = {};
+        $scope.booleanObj = {
+            showSaveBtn : true,
+            showUpdateBtn : false,
+            showCancelBtn : false,
+            showEditBtn : true,
+            showDeleteBtn : true
+        };
+    };
+
+    // Search on the basis of name and email only
+	$scope.search = function(item) {
+		if((item.firstName.toLowerCase()).indexOf($scope.searchText.toLowerCase()) !== -1 ||
+			item.emailValue.indexOf($scope.searchText) !== -1) {
+			return true;
+		}
+	};
+
+
+	// On scroll show back to top button
+	$(window).scroll(function(){
+		if ($(window).scrollTop() <= 380) {
+			$('.back-to-top').fadeOut();
+		}
+		else {
+			$('.back-to-top').fadeIn();
+		}
+	});
+
+	// Scroll to top function
+	$scope.scrollToTop = function() {
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+	};
+
+	function init(){
+		$scope.studentArr = JSON.parse(localStorage.getItem("studentArr")) || [];
+		// For carausal
+		$('.carousel').carousel();
+	}
+	init();
+
+}]).directive('onFinishRender', function ($timeout) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attr) {
+			if (scope.$last === true) {
+				$timeout(function () {
+					scope.$emit(attr.onFinishRender);
+				});
+			}
+		}
+	}
+});
+
+
+app.controller('userCtrl', ['$scope', '$http',  function($scope, $http) {
+	$scope.userHeading = "Here comes the user entries";
+
+	$http({
+		method: 'GET',
+		url: 'https://reqres.in/api/users'
+	}).then(function successCB(response){
+
+		console.log(response.data);
+		$scope.users = response.data.data;
+
+	}, function errorCB(response){
+		$scope.myError = response.data;
+		console.log($scope.myError );
+	});
+
+}]);
---
+++ site/js/indexService.js
@@ -0,0 +1,24 @@
+var app = angular.module('commonService',[]);
+app.service('indexService', function() {
+
+    // main heading(for data sharing)
+	this.mainHeading = "Please fill the informations";
+
+    // Save service
+    this.save = function(tempArr, tempObj){
+        tempArr.push(tempObj);
+        return tempArr;
+    }
+
+    // Delete service
+    this.delete = function(tempArr, tempIndex) {
+        tempArr.splice(tempIndex, 1);
+        return tempArr;
+    }
+
+    // Update service
+    this.update = function(tempArr, tempObj, tempIndex) {
+        tempArr[tempIndex] = tempObj;
+        return tempArr;
+    }
+});
