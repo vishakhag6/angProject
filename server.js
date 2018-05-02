f34d34ddbcd287bc93912c7d8528a9f51d40645d
Nehe Vishakha <vishakhag6@gmail.com>
2018-05-02 14:38:00 +0530

    server file added and API integration

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('port', PORT);
+
+app.use(express.static(__dirname + '/site'));
+
+app.get("/",function(req,res){
+	return res.redirect("/index.html");
+})
+
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Nehe Works' + " " + port);
+});
